47e44f69f84a979f7cc65559af1a39c495bc2c6b
Vixo341 <35233021+Vixo341@users.noreply.github.com>
2023-12-05 18:01:53 +0100

    Add project files.

---
+++ HRManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using HRManagement.Domain.Common;
+
+namespace HRManagement.Domain;
+
+public class LeaveAllocation : BaseEntity
+{
+    public int NumberOfDays { get; set; }
+    public LeaveTypes? LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+    public int Period { get; set; }
+}
---
+++ HRManagement.Domain/LeaveTypes.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using HRManagement.Domain.Common;
+
+namespace HRManagement.Domain;
+
+public class LeaveTypes : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    
+    public int DefaultDays { get; set; }
+}
